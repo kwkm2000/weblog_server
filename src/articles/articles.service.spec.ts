e77ad177448074902292147e84a564b61c1e1e41
kwkm2000 <kwkm.0802@gmail.com>
2023-03-14 00:55:01 +0900

    createのテストを実装

--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -1,86 +1,125 @@
-import { Test } from "@nestjs/testing";
+import { Test, TestingModule } from "@nestjs/testing";
+import { ArticlesService } from "./articles.service";
 import { getRepositoryToken } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
 import { ArticleEntity } from "./article.entity";
 import { TagEntity } from "../tags/tag.entity";
-import { ArticlesService } from "./articles.service";
-import { Article } from "./interface";
 
 describe("ArticlesService", () => {
-  let articlesService: ArticlesService;
-  let articleRepository: Repository<ArticleEntity>;
-  let tagRepository: Repository<TagEntity>;
+  let service: ArticlesService;
+  let articleRepositoryMock: any;
+  let tagRepositoryMock: any;
 
   beforeEach(async () => {
-    const module = await Test.createTestingModule({
+    articleRepositoryMock = {
+      find: jest.fn(),
+      findOneBy: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
+    tagRepositoryMock = {
+      findOneBy: jest.fn(),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
       providers: [
         ArticlesService,
         {
           provide: getRepositoryToken(ArticleEntity),
-          useClass: Repository,
+          useValue: articleRepositoryMock,
         },
         {
           provide: getRepositoryToken(TagEntity),
-          useClass: Repository,
+          useValue: tagRepositoryMock,
         },
       ],
     }).compile();
 
-    articlesService = module.get<ArticlesService>(ArticlesService);
-    articleRepository = module.get<Repository<ArticleEntity>>(
-      getRepositoryToken(ArticleEntity)
-    );
-    tagRepository = module.get<Repository<TagEntity>>(
-      getRepositoryToken(TagEntity)
-    );
+    service = module.get<ArticlesService>(ArticlesService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   describe("create", () => {
-    const testId = 1;
-    const createArticleDto = {
-      title: "Test Article",
-      text: '{"blocks":[{"key":"8o6ur","text":"New Article","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-      tagIds: [1, 2, 3],
-    };
-    const now = new Date();
-    const tag1 = new TagEntity();
-    tag1.id = 1;
-    tag1.label = "Tag1";
-    const article: Article = {
-      id: testId,
-      title: createArticleDto.title,
-      text: JSON.parse(createArticleDto.text),
-      createdAt: now,
-      updatedAt: now,
-      tags: [tag1],
-    };
+    it("記事の作成", async () => {
+      const createArticleDto = {
+        title: "Test article",
+        text: JSON.stringify({
+          blocks: [
+            {
+              key: "8o6ur",
+              text: "New Article",
+              type: "unstyled",
+              depth: 0,
+              inlineStyleRanges: [],
+              entityRanges: [],
+              data: {},
+            },
+          ],
+          entityMap: {},
+        }),
+        tagIds: [1, 2, 3],
+      };
 
-    it("should create an article", async () => {
-      const tag1 = new TagEntity();
-      tag1.id = 1;
-      tag1.label = "Tag1";
+      const tagEntity1 = new TagEntity();
+      tagEntity1.id = 1;
+      tagEntity1.label = "Tag 1";
 
-      jest.spyOn(tagRepository, "findOneBy").mockResolvedValueOnce(tag1);
+      const tagEntity2 = new TagEntity();
+      tagEntity2.id = 2;
+      tagEntity2.label = "Tag 2";
 
-      const createdArticle = new ArticleEntity();
+      const tagEntity3 = new TagEntity();
+      tagEntity3.id = 3;
+      tagEntity3.label = "Tag 3";
 
-      createdArticle.id = 1;
-      createdArticle.title = createArticleDto.title;
-      createdArticle.text = createArticleDto.text;
-      createdArticle.createdAt = new Date();
-      createdArticle.updatedAt = new Date();
-      createdArticle.tags = [tag1];
+      tagRepositoryMock.findOneBy.mockResolvedValueOnce(tagEntity1);
+      tagRepositoryMock.findOneBy.mockResolvedValueOnce(tagEntity2);
+      tagRepositoryMock.findOneBy.mockResolvedValueOnce(tagEntity3);
 
-      jest
-        .spyOn(articleRepository, "save")
-        .mockResolvedValueOnce(createdArticle);
+      const articleEntity = new ArticleEntity();
+      articleEntity.id = 1;
+      articleEntity.title = createArticleDto.title;
+      articleEntity.text = createArticleDto.text;
+      articleEntity.createdAt = new Date();
+      articleEntity.updatedAt = new Date();
+      articleEntity.tags = [tagEntity1, tagEntity2, tagEntity3];
 
-      const result = await articlesService.create(createArticleDto);
-      expect(result).toEqual(article);
-      // expect(tagRepository.findOneBy).toHaveBeenCalledTimes(3);
-      expect(articleRepository.save).toHaveBeenCalledTimes(1);
+      articleRepositoryMock.save.mockResolvedValueOnce(articleEntity);
+
+      const result = await service.create(createArticleDto);
+
+      expect(tagRepositoryMock.findOneBy).toHaveBeenCalledTimes(3);
+      expect(articleRepositoryMock.save).toHaveBeenCalledWith(
+        expect.any(ArticleEntity)
+      );
+      expect(result).toEqual({
+        id: articleEntity.id,
+        title: articleEntity.title,
+        text: JSON.parse(articleEntity.text),
+        createdAt: articleEntity.createdAt,
+        updatedAt: articleEntity.updatedAt,
+        tags: articleEntity.tags,
+      });
     });
   });
-
-  // 他のメソッドに対するテストも同様に実装できます
 });
+// describe('update', () => {
+//   it('should update an article', async () => {
+//     const id = 1;
+//     const updateArticleDto = {
+//       title: 'Test article (updated)',
+//       text: 'This is an updated test article.',
+//     };
+
+//     const articleEntity = new ArticleEntity();
+//     articleEntity.id = id;
+//     articleEntity.title = updateArticleDto.title;
+//     articleEntity.text = updateArticleDto.text;
+//     articleEntity.createdAt = new Date();
+//     articleEntity.updatedAt = new Date();
+
+//     articleRepositoryMock.findOneBy.mockResolvedValueOnce(articleEntity);
+//     articleRepositoryMock.save.mockResolvedValueOnce(article
