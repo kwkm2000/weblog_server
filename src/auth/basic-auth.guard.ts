f600775468f8d591e88a093c2c8d32d13c76ebeb
kwkm2000 <kwkm.0802@gmail.com>
2023-04-03 16:03:08 +0900

    envから読み込む

--- src/auth/basic-auth.guard.ts
+++ src/auth/basic-auth.guard.ts
@@ -1,13 +1,24 @@
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
 import { Request, Response } from "express";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class BasicAuthGuard implements CanActivate {
+  username: string;
+  password: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.username = configService.get<string>("BASIC_AUTH_USERNAME");
+    this.password = configService.get<string>("BASIC_AUTH_PASSWORD");
+  }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log("this.username", this.username);
+    console.log("this.password", this.password);
     const request = context.switchToHttp().getRequest<Request>();
     const response = context.switchToHttp().getResponse<Response>();
     const authorizationHeader = request.headers.authorization;
-    console.log("authorizationHeader", authorizationHeader);
+
     if (!authorizationHeader) {
       response.setHeader("WWW-Authenticate", 'Basic realm="Secure Area"');
       response.status(401).send({ message: "Unauthorized" });
@@ -24,12 +35,8 @@ export class BasicAuthGuard implements CanActivate {
 
     const [username, password] = credentials.split(":");
 
-    console.log("username", username);
-    console.log("password", password);
-
-    // ここでユーザー名とパスワードを検証します。データベースやハードコードされた値を使用できます。
     const isAuthenticated =
-      username === "your-username" && password === "your-password";
+      username === this.username && password === this.password;
 
     if (!isAuthenticated) {
       response.setHeader("WWW-Authenticate", 'Basic realm="Secure Area"');
